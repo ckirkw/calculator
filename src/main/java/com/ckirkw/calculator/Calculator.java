4f24941409184bb6bbd4c229a0376e3964e6eb4c
Cresta Kirkwood <cresta.kirkwood@gmail.com>
2021-03-07 16:03:43 -0800

    Attempted to add hazelcast. Not working

--- src/main/java/com/ckirkw/calculator/Calculator.java
+++ src/main/java/com/ckirkw/calculator/Calculator.java
@@ -1,10 +1,19 @@
 package com.ckirkw.calculator;
 
 import org.springframework.stereotype.Service;
+import org.springframework.cache.annotation.Cacheable;
 
 @Service
 public class Calculator {
+  @Cacheable("sum")
   int sum(int a, int b) {
+    try {
+      Thread.sleep(3000);
+    }
+    catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    
     return a + b;
   }
 }
